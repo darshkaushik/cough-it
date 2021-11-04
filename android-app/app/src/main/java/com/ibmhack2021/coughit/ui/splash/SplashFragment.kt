16176adc4c24222448e0f5a32d22fbdd647c1232
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-11-04 12:36:23 +0530

    1. Splash screen configuration complete

--- android-app/app/src/main/java/com/ibmhack2021/coughit/ui/splash/SplashFragment.kt
+++ android-app/app/src/main/java/com/ibmhack2021/coughit/ui/splash/SplashFragment.kt
@@ -1,11 +1,16 @@
 package com.ibmhack2021.coughit.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.google.android.material.transition.MaterialElevationScale
 import com.ibmhack2021.coughit.R
+import com.ibmhack2021.coughit.databinding.FragmentSplashBinding
 
 
 private const val ARG_PARAM1 = "param1"
@@ -16,6 +21,9 @@ class SplashFragment : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +38,23 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        // your code goes here
+        exitTransition = MaterialElevationScale(true)
+
+        // here I have to call firebase and then verify if the user has signed in
+        // if the user has signed in already then move to home fragment directlly
+        // todo : add firebase logic here
+
+        // move to next screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.root.findNavController().navigate(R.id.action_splashFragment_to_signUpFragment)
+        }, 600)
+
+
+
+
+        return binding.root
     }
 
     companion object {
@@ -43,5 +67,10 @@ class SplashFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
