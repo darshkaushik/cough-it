1c148aee20e8a18feb3674a71e0961e7c8e8fcb3
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-11-06 16:51:56 +0530

    1. Completed the Home Layout Fragment

--- android-app/app/src/main/java/com/ibmhack2021/coughit/ui/home/HomeFragment.kt
+++ android-app/app/src/main/java/com/ibmhack2021/coughit/ui/home/HomeFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.transition.MaterialElevationScale
 import com.ibmhack2021.coughit.R
+import com.ibmhack2021.coughit.databinding.FragmentHomeBinding
+import com.ibmhack2021.coughit.databinding.FragmentSplashBinding
 
 
 private const val ARG_PARAM1 = "param1"
@@ -16,6 +19,9 @@ class HomeFragment : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +36,32 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        exitTransition = MaterialElevationScale(true)
+        enterTransition = MaterialElevationScale(true)
+
+
+        handleFABOnScroll()
+
+
+
+        return binding.root
+    }
+
+    fun handleFABOnScroll(){
+        binding.run {
+            nestedScrollView.setOnScrollChangeListener {
+                    v, scrollX, scrollY, oldScrollX, oldScrollY ->
+                if (scrollY > oldScrollY)
+                    testFab.shrink()
+                else if(scrollX == scrollY)
+                    testFab.extend()
+                else testFab.extend()
+
+
+            }
+        }
     }
 
     companion object {
