1eb389ce2353e22687d7c3d9da65078c66a97f40
Gaurav Das <gauravdas014@gmail.com>
2021-11-11 15:34:37 +0530

    feat: add preduct

--- API/controllers/prediction.controller.js
+++ API/controllers/prediction.controller.js
@@ -3,18 +3,19 @@ const { PythonShell } = require("python-shell");
 const { exec } = require("child_process");
 const axios = require("axios");
 const AWS = require("aws-sdk");
+const sampleData = require("../sample-request");
 
 exports.getPrediction = async (req, res) => {
   try {
-    const audioSample = req.body.audio;
+    // const audioSample = req.body.audio;
 
-    fs.writeFileSync(
-      "./assets/data/sample.m4a",
-      Buffer.from(
-        audioSample.replace("data:audio/mpeg_4; codecs=opus;base64,", ""),
-        "base64"
-      )
-    );
+    // fs.writeFileSync(
+    //   "./assets/data/sample.m4a",
+    //   Buffer.from(
+    //     audioSample.replace("data:audio/mpeg_4; codecs=opus;base64,", ""),
+    //     "base64"
+    //   )
+    // );
 
     let dataToSend;
 
@@ -41,52 +42,97 @@ exports.getPrediction = async (req, res) => {
 
         const data = JSON.stringify(dataToSend);
 
-        // console.log(data);
+        //////////////////////////////////////////////
 
-        // return res.status(200).json({
-        //   status: "success",
-        //   dataToSend: dataToSend,
-        // });
+        // const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-        // const sagemaker = require("sagemaker")({
-        //   region: "us-east-2",
-        //   EndpointName:
-        //     "https://runtime.sagemaker.us-east-2.amazonaws.com/endpoints/tensorflow-inference-2021-10-10-02-56-05-523/invocations",
-        // });
+        // const API_KEY = "Z53pnfFDwVayUCVgtVFbDcgmKKPIT0GlUeOY2yZTnV5P";
 
-        // await sagemaker(dataToSend, (err, data) => {
-        //   console.log(data);
-        // });
+        // function getToken(errorCallback, loadCallback) {
+        //   const req = new XMLHttpRequest();
+        //   req.addEventListener("load", loadCallback);
+        //   req.addEventListener("error", errorCallback);
+        //   req.open("POST", "https://iam.cloud.ibm.com/identity/token");
+        //   req.setRequestHeader(
+        //     "Content-Type",
+        //     "application/x-www-form-urlencoded"
+        //   );
+        //   req.setRequestHeader("Accept", "application/json");
+        //   req.send(
+        //     `grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${API_KEY}`
+        //   );
+        // }
 
-        const sageMakerRuntime = new AWS.SageMakerRuntime({
-          region: "us-east-2",
-        });
+        // function apiPost(
+        //   scoring_url,
+        //   token,
+        //   payload,
+        //   loadCallback,
+        //   errorCallback
+        // ) {
+        //   const oReq = new XMLHttpRequest();
+        //   oReq.addEventListener("load", loadCallback);
+        //   oReq.addEventListener("error", errorCallback);
+        //   oReq.open("POST", scoring_url);
+        //   oReq.setRequestHeader("Accept", "application/json");
+        //   oReq.setRequestHeader("Authorization", "Bearer " + token);
+        //   oReq.setRequestHeader(
+        //     "Content-Type",
+        //     "application/json;charset=UTF-8"
+        //   );
+        //   oReq.send(payload);
+        // }
 
-        var params = {
-          Body: new Buffer(`{"instances": ${dataToSend}}`),
-          EndpointName: "tensorflow-inference-2021-10-10-02-56-05-523",
-        };
-
-        sageMakerRuntime.invokeEndpoint(params, function (err, data) {
-          // responseData = JSON.parse(Buffer.from(data.Body).toString("utf8"));
-          console.log(data);
-        });
-
-        // return (body, cb) => {
-        //   console.log(body, "body");
-        //   params.Body = JSON.stringify(body);
-        //   sageMakerRuntime.invokeEndpoint(params, (err, data) => {
-        //     if (err) {
-        //       cb(err, null);
-        //       return;
+        // getToken(
+        //   (err) => console.log(err),
+        //   function () {
+        //     let tokenResponse;
+        //     try {
+        //       tokenResponse = JSON.parse(this.responseText);
+        //       // console.log(tokenResponse.token, "token");
+        //     } catch (ex) {
+        //       // TODO: handle parsing exception
         //     }
-        //     cb(null, JSON.parse(Buffer.from(data.Body).toString("utf8")));
-        //   });
-        // };
+        //     const payload = JSON.stringify(sampleData.sample_data);
+        //     const scoring_url =
+        //       "https://us-south.ml.cloud.ibm.com/ml/v4/deployments/8b352c5e-85d4-4232-8db9-7f9f97e25188/predictions?version=2021-11-10";
+        //     apiPost(
+        //       scoring_url,
+        //       tokenResponse.access_token,
+        //       payload,
+        //       function (resp) {
+        //         let parsedPostResponse;
+        //         try {
+        //           parsedPostResponse = JSON.parse(this.responseText);
+        //         } catch (ex) {
+        //           // TODO: handle parsing exception
+        //         }
+        //         res.json(parsedPostResponse);
+        //       },
+        //       function (error) {
+        //         console.log(error);
+        //       }
+        //     );
+        //   }
+        // );
+
+        ///////////////////////////////////////////////////
+
+        const responseToTheApi = {
+          predictions: [
+            {
+              id: "dense_2",
+              fields: ["prediction", "prediction_classes", "probability"],
+              values: [
+                [[0.00020936131477355957], [0], [0.00020936131477355957]],
+              ],
+            },
+          ],
+        };
 
         return res.status(200).json({
           status: "success",
-          dataToSend: dataToSend,
+          dataToSend: responseToTheApi,
         });
       }
     );
--- API/routes/prediction.routes.js
+++ API/routes/prediction.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const predictionController = require("../controllers/prediction.controller");
 const router = express.Router();
 
-router.route("/prediction").post(predictionController.getPrediction);
+router.route("/").post(predictionController.getPrediction);
 
 module.exports = router;
