9794b8fdf2bef8f421462bb06af385d5e1fe1ce1
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-11-14 20:36:36 +0530

    update : button functionality added

--- android-app/app/src/main/java/com/ibmhack2021/coughit/ui/home/HomeFragment.kt
+++ android-app/app/src/main/java/com/ibmhack2021/coughit/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.ibmhack2021.coughit.ui.home
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -200,6 +202,12 @@ class HomeFragment : Fragment(), EasyPermissions.PermissionCallbacks {
             // account icon set up
             accountIcon.setOnClickListener {
                 findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+            }
+
+            moreInfoButton.setOnClickListener {
+                val uri = Uri.parse("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public")
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
             }
         }
 
