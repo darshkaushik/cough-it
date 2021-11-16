eab19e2775c6b092bcff0bc446fb092ace875cc4
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-11-16 20:48:02 +0530

    enhancements: refined the prediction page.

--- android-app/app/src/main/java/com/ibmhack2021/coughit/ui/prediction/PredictionFragment.kt
+++ android-app/app/src/main/java/com/ibmhack2021/coughit/ui/prediction/PredictionFragment.kt
@@ -102,6 +102,15 @@ class PredictionFragment : Fragment() {
             // here I can set the text view
             Log.d("Prediction" , it.toString())
             binding.predictTextView.text = String.format("%.3f", it?.times(100))
+
+            // here I have to set the categories
+            if(it?.times(100)!! > 90){
+                binding.categoryText.text = "you are at a high chance of being infected consider consulting a doctor immediately"
+            }else if(it?.times(100) > 40 && it.times(100) < 90){
+                binding.categoryText.text = "You are at medium risk, try to avoid contact from others and keep regularly testing"
+            }else{
+                binding.categoryText.text = "You are at low risk of infection. always wear a mask to avoid infection in future and keep testing"
+            }
         })
 
         // navigate to home
